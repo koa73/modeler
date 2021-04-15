154499ae9184c623e44644573908a05a9688a01b
koa73 <koa73@bk.ru>
2021-04-15 22:47:33 +0300

    --- add model_maker for with changing None weight

--- checker.sh
+++ checker.sh
@@ -20,7 +20,7 @@ printf "\r${3} : [${_fill// /#}${_empty// /-}] ${_progress}%%  Until the end app
 _start=0
 # This accounts as the "totalState" variable for the ProgressBar function
 # 147
-_end=201
+_end=21
 # Butch size
 _step=100
 
