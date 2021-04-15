65b6da0e15057447100b88f38a80f3fe3e8f8b74
koa73 <koa73@bk.ru>
2021-04-15 22:25:44 +0300

    --- add model_maker for with changing None weight

--- checker.sh
+++ checker.sh
@@ -20,9 +20,9 @@ printf "\r${3} : [${_fill// /#}${_empty// /-}] ${_progress}%%  Until the end app
 _start=0
 # This accounts as the "totalState" variable for the ProgressBar function
 # 147
-_end=200
+_end=201
 # Butch size
-_step=10
+_step=100
 
 if [ -f ./models/logs/checker_log.csv ]; then
    count_file=$(find ./models/logs/checker_* -type f |wc -l)
