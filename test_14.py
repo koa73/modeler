c1b6bfbdcbf61aef6fd3b2925353574415a18a9d
koa73 <koa73@bk.ru>
2021-04-24 15:27:33 +0300

    ---

--- test_14.py
+++ test_14.py
@@ -1,5 +1,5 @@
 import modelMaker as d
 
-arr = d.get_combinations_name('weights_b25_150_', [70,4,27,56,38], 82)
+arr = d.get_combinations_name('weights_b25_150_', [70,4,27,56,38,76,80], 82)
 print(arr)
 print(len(arr))
